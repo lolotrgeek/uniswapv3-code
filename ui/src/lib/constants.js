895b2f5d713afc6948fb00160031d88fb3dc5976
lolotrgeek <jon@simplyenable.com>
2024-01-26 05:10:15 -0600

    update poolCodeHash

--- ui/src/lib/constants.js
+++ ui/src/lib/constants.js
@@ -8,6 +8,7 @@ const feeToSpacing = {
 }
 
 // forge inspect UniswapV3Pool bytecode| xargs cast keccak
-const poolCodeHash = "0x9dc805423bd1664a6a73b31955de538c338bac1f5c61beb8f4635be5032076a2";
+// const poolCodeHash = "0x9dc805423bd1664a6a73b31955de538c338bac1f5c61beb8f4635be5032076a2";
+const poolCodeHash = "0x0fc90f62c036998e21025e0735a5b2699bde38f5a8a708c79791b0f2697ed36c"
 
 export { uint256Max, feeToSpacing, poolCodeHash };
