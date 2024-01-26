18a64160fb523a3b715b05545b5ef2b251b278a3
lolotrgeek <jon@simplyenable.com>
2024-01-26 15:23:35 -0600

    wallet helpers

---
+++ api/wallet.js
@@ -0,0 +1,20 @@
+import { ethers } from "ethers"
+
+async function newWallet() {
+    const wallet = ethers.Wallet.createRandom()
+    return wallet
+}
+
+async function restoreWallet(pk){
+    const wallet = new ethers.Wallet(pk)
+    return wallet
+}
+
+async function testWallet(){
+    const address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    const pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    const wallet = new ethers.Wallet(pk)
+    return wallet
+}
+
+export { newWallet, restoreWallet, testWallet }
